f0d773f71338616b6706b555237807439dc9073c
doudouu0504 <selinafs880504@gmail.com>
2024-12-12 23:09:58 +0800

    feat: navbar

--- users/views.py
+++ users/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login as login_user, logout as logout_user
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views.decorators.http import require_POST
-
-
-# Create your views here.
 
 
 def register(request):
@@ -16,28 +12,26 @@ def register(request):
 
         if form.is_valid():
             form.save()
-            # messages.success(request, "註冊成功")
-            return redirect("pages:home")
+            messages.success(request, "註冊成功!!!歡迎登入")
+            return redirect("users:login")
         else:
-            return HttpResponse("註冊失敗")
-
+            messages.success(request, "很抱歉，您註冊失敗!!")
+            return redirect("users:register")
     return render(request, "users/register.html")
 
 
 def login(request):
     if request.POST:
-        # 驗證
         user = authenticate(
             username=request.POST.get("username"),
             password=request.POST.get("password"),
         )
 
         if user is not None:
-            login_user(request, user)
-            messages.success(request, "登入成功")
+            login_user(request, user)  # 發號碼牌
             return redirect("pages:home")
         else:
-            messages.success(request, "登入失敗")
+            messages.success(request, "登入失敗，請重新登入一次。")
             return redirect("users:login")
     return render(request, "users/login.html")
 
