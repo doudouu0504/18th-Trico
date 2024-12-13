ae206406c0f1f9128bf8ef46badad72dd51533a4
chentingchieh <happy810502@gmail.com>
2024-12-13 18:57:52 +0800

    add: client.html and freelancer.html

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "pages/home.html")
+
+
+def client(request):
+    return render(request, "pages/client.html")
+
+
+def freelancer(request):
+    return render(request, "pages/freelancer.html")
