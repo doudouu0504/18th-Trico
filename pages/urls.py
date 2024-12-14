5ee8f3df92deec728664f4038b568db14475c204
chentingchieh <happy810502@gmail.com>
2024-12-14 15:20:32 +0800

    add: portfolio_showcase.html

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, portfolio_showcase
 
 app_name = "pages"
 
 urlpatterns = [
     path("", home, name="home"),
+    path("portfolio_showcase/", portfolio_showcase, name="portfolio_showcase"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "pages/home.html")
+
+
+def portfolio_showcase(request):
+    return render(request, "pages/portfolio_showcase.html")
