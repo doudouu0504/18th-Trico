8cbe19ec0362909fead3cb0947688e0c17e89b60
liaojingjie <artfulrachel6302524@gmail.com>
2024-12-19 15:40:27 +0800

    wip: searchpage

--- pages/views.py
+++ pages/views.py
@@ -4,11 +4,18 @@ from django.shortcuts import render
 def home(request):
     return render(request, "pages/home.html")
 
+
 def portfolio_showcase(request):
     return render(request, "pages/portfolio_showcase.html")
-  
+
+
 def client(request):
     return render(request, "pages/client.html")
 
+
 def freelancer(request):
     return render(request, "pages/freelancer.html")
+
+
+def searchpage(request):
+    return render(request, "pages/searchpage.html")
