d8f6fafd2bea2f3f45dc9842d4ff45fb08e44fa2
Michelle Lai <ktlaiktlai@gmail.com>
2024-12-25 00:46:36 +0800

    modify: render archieved data on homepage

--- pages/views.py
+++ pages/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
-
+from services.models import Service  
 
 def home(request):
-    return render(request, "pages/home.html")
-
+    services = Service.objects.order_by("-created_at")[:4]  
+    return render(request, "pages/home.html", {"services": services})
 
 def portfolio_showcase(request):
     return render(request, "pages/portfolio_showcase.html")
 
-
 def client(request):
     return render(request, "pages/client.html")
 
-
 def freelancer(request):
     return render(request, "pages/freelancer.html")
-
