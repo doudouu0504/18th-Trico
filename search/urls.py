a1555c49832479d2af7c6e37f3ecbf03f401cf2f
liaojingjie <artfulrachel6302524@gmail.com>
2025-01-06 14:28:24 +0800

    feat: search

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.search_view, name="search"),
+    path("search", views.search_view, name="search"),
 ]
