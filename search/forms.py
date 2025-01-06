61c2b60fe8aed6b692799c52b8e6c108df968dbc
liaojingjie <artfulrachel6302524@gmail.com>
2025-01-06 16:46:39 +0800

    fix: searchbug

--- search/forms.py
+++ search/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    keyword = forms.CharField(max_length=50, required=False, label="搜尋關鍵字")
+    keyword = forms.CharField(
+        max_length=50,
+        required=True,
+        widget=forms.TextInput(attrs={"placeholder": "請輸入關鍵字..."}),
+    )
--- search/models.py
+++ search/models.py
@@ -8,3 +8,6 @@ class Search(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"pk": self.id})
