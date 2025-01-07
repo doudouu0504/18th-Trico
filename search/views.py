cf889321b164cd82966c9c3962830537b5909c18
liaojingjie <artfulrachel6302524@gmail.com>
2025-01-07 10:38:58 +0800

    fix: searchbug

--- search/views.py
+++ search/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Q
-from .models import Search
+from services.models import Service
 from .forms import SearchForm
 
 
@@ -16,8 +16,8 @@ def search_view(request):
         if form.is_valid():
             query = form.cleaned_data["keyword"]
             # 過濾標題或內容中包含關鍵字的結果
-            results = Search.objects.filter(
-                Q(title__icontains=query) | Q(content__icontains=query)
+            results = Service.objects.filter(
+                Q(title__icontains=query) | Q(description__icontains=query)
             )
     else:
         form = SearchForm()
